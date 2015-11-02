6527f78eb29cf017a2c425b74a336738da8b42ad
liwzhi <liwzhi@gmail.com>
2015-11-01 23:11:41 -0800

    add java  solution

--- everStringProject/kaggle_stock_prediction.py
+++ everStringProject/kaggle_stock_prediction.py
@@ -3,6 +3,23 @@
 Created on Sat Oct 31 16:05:54 2015
 
 @author: weizhi
+
+
+train.csv - the training set, including the columns of:
+Feature_1 - Feature_25
+Ret_MinusTwo, Ret_MinusOne
+Ret_2 - Ret_120
+Ret_121 - Ret_180: target variables
+Ret_PlusOne, Ret_PlusTwo: target variables
+Weight_Intraday, Weight_Daily
+
+
+test.csv - the test set, including the columns of:
+Feature_1 - Feature_25
+Ret_MinusTwo, Ret_MinusOne
+Ret_2 - Ret_120
+sample_submission.csv - a sample submission file in the correct format
+
 """
 
 import pandas as pd
